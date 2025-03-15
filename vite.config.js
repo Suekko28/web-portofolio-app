5c1e0e0bb9fdf5ffcd4b4747c14fc00d024ab3ad
Aldi Dharmawan <aldidharmawan700@gmail.com>
2025-03-15 22:24:33 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // base: process.env.VITE_BASE_PATH || "/react-vite-deploy", 
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets/images", // Folder gambar dalam src
+          dest: "assets", // Folder tujuan dalam build
+        },
+        {
+          src: "src/assets/video",
+          dest: "assets"
+        }
+      ],
+    }),
+  ],
 });
