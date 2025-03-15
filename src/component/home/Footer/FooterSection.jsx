2c184d1bb3832eb677f42b9526f5480f137fb33f
Aldi Dharmawan <aldidharmawan700@gmail.com>
2025-03-15 23:16:33 +0700

    Update FooterSection.jsx

--- src/component/home/Footer/FooterSection.jsx
+++ src/component/home/Footer/FooterSection.jsx
@@ -3,7 +3,7 @@ import FooterItem from "./FooterItem";
 
 function FooterSection() {
   return (
-    <div className="footer-section bg-blue-footer py-6">
+    <div className="footer-section bg-blue-footer py-6 h-[300px] md:h-fit">
       <div className="mx-[32px] xl:mx-[120px] custome_margin">
         <FooterItem />
       </div>
