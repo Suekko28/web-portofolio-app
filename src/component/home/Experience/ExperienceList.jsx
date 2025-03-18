9cac53ccbd716aa40cafc06c118219f1ef78e92b
Aldi Dharmawan <aldidharmawan700@gmail.com>
2025-03-18 16:21:05 +0700

    Update ExperienceList.jsx

--- src/component/home/Experience/ExperienceList.jsx
+++ src/component/home/Experience/ExperienceList.jsx
@@ -6,7 +6,7 @@ function ExperienceList() {
     <div className="experience-list" id="experiences">
       <div className="mt-64 md:mt-64 mx-[32px] xl:mx-[120px] custome_margin_experience_list">
         <h2 className="text-2xl font-bold text-center mb-[16px] text-blue-dark">
-          Experience
+          Experiences
         </h2>
         <hr className="h-2 bg-blue-divider rounded w-[64px] mb-[32px] mx-auto" />
         <ExperienceItem />
